0e23a860f35ef1899a720ad789d66a261e7806d3
Daniel Gomez Aristizabal <99361024+DaniGomAris@users.noreply.github.com>
2023-11-12 01:20:07 -0500

    Create Book.py

---
+++ Book.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title, writer, price, writer, pages, gender, published, gender):
+        self.title = title
+        self.writer = writer
+        self.price = price
+        self.value = value
+        self.pages = pages
+        self.gender = gender
+        self.published = published
