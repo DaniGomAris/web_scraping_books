3da9021ac8f6a37dfad13db84ccefd528b47068f
Daniel Gomez <danielgomezaristizabal2020@gmail.com>
2023-11-15 21:25:42 -0500

    Guardar la información en un archivo JSON

--- Scrapper.py
+++ Scrapper.py
@@ -22,9 +22,31 @@ def scrapper_books_page():
 
         for row in rows:
             book_link = row.find('a', class_="bookTitle")  
-            #print(book_link.text) # Imprimir nombres de los libros
+            print(book_link.text) # Imprimir nombres de los libros
             href = "https://www.goodreads.com/" + book_link['href']
             book_info = scrapper_books_info(href)
             books_list.append(book_info)
-        
+
+    # Convertir la información de los libros a una lista de diccionarios
+    books_data = []
+    for book in books_list:
+        book_data = {
+            "title": book.title,
+            "author": book.author,
+            "published": book.published,
+            "price": book.price,
+            "valuation": book.valuation,
+            "pages": book.pages,
+            "link": book.link,
+            "gender": book.gender
+        }
+        books_data.append(book_data)
+
+    # Guardar la información en un archivo JSON
+    with open('books.json', 'w', encoding='utf-8') as json_file:
+        json.dump(books_data, json_file, ensure_ascii=False, indent=4)
+
+    print(f"La información de las páginas 1 a 3 se ha guardado en books.json")
+    return books_list
+   
 scrapper_books_page()
